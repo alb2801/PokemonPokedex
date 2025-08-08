ee15654546bf7c3709ac606463234088c136e2b9
alb2801 <alberthpulgarin@gmail.com>
2025-08-08 16:48:51 -0500

    Creación home

---
+++ src/app/pages/component/info-pokemon/info-pokemon.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-info-pokemon',
+  imports: [],
+  templateUrl: './info-pokemon.html',
+  styleUrl: './info-pokemon.css'
+})
+export class InfoPokemon {
+
+}
