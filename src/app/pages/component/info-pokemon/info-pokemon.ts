24a7d631efae09eecbe548c0d105dc6082166803
alb2801 <alberthpulgarin@gmail.com>
2025-08-14 09:36:00 -0500

    Buscar y card funcionando

--- src/app/pages/component/info-pokemon/info-pokemon.ts
+++ src/app/pages/component/info-pokemon/info-pokemon.ts
@@ -1,11 +1,25 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, Input, OnChanges, SimpleChanges } from '@angular/core';
+import { MatCardModule } from '@angular/material/card';
 
 @Component({
   selector: 'app-info-pokemon',
-  imports: [],
+  imports: [CommonModule, MatCardModule],
   templateUrl: './info-pokemon.html',
-  styleUrl: './info-pokemon.css'
+  styleUrl: './info-pokemon.css',
 })
-export class InfoPokemon {
+export class InfoPokemon implements OnChanges {
+  @Input() pokemon: any = null;
+  tipos: string = '';
+  habilidades = '';
 
+  ngOnChanges(changes: SimpleChanges): void {
+    if (changes['pokemon'] && this.pokemon) {
+      this.tipos = this.pokemon.types.map((t: any) => t.type.name).join(', ');
+
+      this.habilidades = this.pokemon.abilities
+        .map((a: any) => a.ability.name)
+        .join(', ');
+    }
+  }
 }
--- src/app/pages/search/search.ts
+++ src/app/pages/search/search.ts
@@ -1,11 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Output } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-search',
-  imports: [],
+  imports: [CommonModule ,MatInputModule, MatButtonModule, FormsModule],
   templateUrl: './search.html',
   styleUrl: './search.css'
 })
 export class Search {
+  @Output() search = new EventEmitter<string>();
+  searchName = '';
 
+  buscarPokemon() {
+    if (this.searchName.trim()) {
+      this.search.emit(this.searchName);
+    }
+  }
 }
